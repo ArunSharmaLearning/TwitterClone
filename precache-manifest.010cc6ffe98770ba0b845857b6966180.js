b6a91c7276fddb1ab3da2857c0696bcb4f70a929
Arun sharma <arunlivingfantastico@gmail.com>
2020-09-11 12:17:14 +0530

    Updates

---
+++ precache-manifest.010cc6ffe98770ba0b845857b6966180.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47f12487fe3e40cfff13453072fe009a",
+    "url": "/TwitterClone/index.html"
+  },
+  {
+    "revision": "79886116a02d92d12ea1",
+    "url": "/TwitterClone/static/css/main.3c15036b.chunk.css"
+  },
+  {
+    "revision": "be27a31f36cf3109bdec",
+    "url": "/TwitterClone/static/js/2.67eb40c0.chunk.js"
+  },
+  {
+    "revision": "a8de4128b679d6159aad8871b5569c6d",
+    "url": "/TwitterClone/static/js/2.67eb40c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79886116a02d92d12ea1",
+    "url": "/TwitterClone/static/js/main.e24c3319.chunk.js"
+  },
+  {
+    "revision": "12d66213cd89ac09ca95",
+    "url": "/TwitterClone/static/js/runtime-main.9732573d.js"
+  }
+]);
